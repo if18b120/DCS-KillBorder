1944953db55702cf49eee9df256df89681520fd8
if18b120 <bastlxfrank@gmail.com>
2022-10-06 16:48:21 +0200

    border marking on map changed to flexiborder

--- KillBorder.lua
+++ KillBorder.lua
@@ -13,41 +13,6 @@ KillBorder = {
 	sound
 }
 
---[[
-Version History
-2.0.0 - initial KillBorder rework
-2.1.0 - fixed distance calculation, added weapon killborder support, some changes to the mini manual
-2.2.0 - changed loop to be event based, several removals of undefined behaviour, corrections in the mini manual
-2.2.1 - removed debug message, worst bug ever fixed
-2.2.2a - experimental sound support
-
-Mini Manual
-variables not mentioned here are internally managed and should not be fucked with
-
-ReferenceGroup = the group used to generate the border, 2 units required, relative position of the units doesnt matter
-
-coalition/Coalition = 0 check all coalitions
-coalition/Coalition = 1 check just red coalition
-coalition/Coalition = 2 check just blu coalition
-
-side/Side = 1 kill everything above the border
-side/Side = -1 kill everything below the border
-
-groupIdentifier/GroupIdentifier = "String" targeted group names must contain this
-
-checkTime/Time = seconds delay between border checks
-
-warnDistance/WarnDistance = the distance at which a warning text will be displayed, distance function was wonky but is fixed now, measurements is most likely in metres
-
-punishType/PunishType = 0 tiny explosion every few seconds
-punishType/PunishType = 1 instant kill
-punishType/PunishType = 2 back to spectators, not supported yet
-
-punishTimer/PunishTimer = time between punishments if punishType == 0
-
-sound/Sound = soundfile to be played, has to be in the mission file with the following path l10n/DEFAULT/file, ogg and wav files should be possible
---]]
-
 function KillBorder:generate(ReferenceGroup, Coalition, Side, GroupIdentifier, Time, WarnDistance, PunishType, PunishTimer, Sound)
 	-- trigger.action.outText("running", 10, false)
 	self.coalition = Coalition
@@ -60,30 +25,22 @@ function KillBorder:generate(ReferenceGroup, Coalition, Side, GroupIdentifier, T
 	self.sound = "l10n/DEFAULT/" .. Sound
 	
 	local units = Group.getByName(ReferenceGroup):getUnits()
-
-	-- local x1
-	-- local y1
-
-	-- if units[1]:getPoint().z < units[2]:getPoint().z then
-	-- 	x1 = units[1]:getPoint().z
-	-- 	y1 = units[1]:getPoint().x
-	-- 	x2 = units[2]:getPoint().z
-	-- 	y2 = units[2]:getPoint().x
-	-- else
-	-- 	x2 = units[1]:getPoint().z
-	-- 	y2 = units[1]:getPoint().x
-	-- 	x1 = units[2]:getPoint().z
-	-- 	y1 = units[2]:getPoint().x
-	-- end
-
-	-- self.border.a = (y2-y1)/(x2-x1)
-	-- self.border.c = y1 - self.border.a * x1
-
-	-- trigger.action.lineToAll(-1, 69420, {x = self.border.a * 1000000 + self.border.c, y = 500, z = 1000000}, {x = self.border.a * -1000000 + self.border.c, y = 500, z = -1000000}, {1, 0, 0, 1}, 1, true)
-	-- trigger.action.textToAll(-1, 42069, {x = units[1]:getPoint().x - 6000, y = units[1]:getPoint().y, z = units[1]:getPoint().z + 6000}, {1, 0, 0, 1}, {1, 0, 0, 0}, 20, true, 'PVP Border')
+	local count = 0
 	for key, value in units do
 		self.flexiborder.insert(value:getPoint())
+		count = count + 1
 	end
+	local V = getV2(self.flexiborder[2], self.flexiborder[1])
+	self.flexiborder[1] = {x = V.x * 1000000 + self.flexiborder[2].x, z = V.z * 1000000 + self.flexiborder[2].z}
+
+	V = getV2(self.flexiborder[count - 1], self.flexiborder[count])
+	self.flexiborder[count] = {x = V.x * 1000000 + self.flexiborder[count - 1].x, z = V.z * 1000000 + self.flexiborder[count - 1].z}
+
+	local i = 1
+	while i < count do
+		trigger.action.lineToAll(-1, i + 1000, self.flexiborder[i], self.flexiborder[i + 1], {1, 0, 0, 1}, 1, true)
+	end
+	trigger.action.textToAll(-1, 1000, {x = self.flexiborder[2].x - 6000, y = 0, z = self.flexiborder[2].z + 6000}, {1, 0, 0, 1}, {1, 0, 0, 0}, 20, true, 'PVP Border')
 end
 
 function KillBorder.playerLoop(args)
