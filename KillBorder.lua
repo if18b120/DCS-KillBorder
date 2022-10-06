c887dbdfa8a1db1e71c3bbbb7530c96bde6e9d60
if18b120 <48284379+if18b120@users.noreply.github.com>
2022-10-06 23:57:46 +0200

    bugfixes, everything works now

--- KillBorder.lua
+++ KillBorder.lua
@@ -26,19 +26,23 @@ function KillBorder:generate(ReferenceGroup, Coalition, Side, GroupIdentifier, T
 	
 	local units = Group.getByName(ReferenceGroup):getUnits()
 	local count = 0
-	for key, value in units do
-		self.flexiborder.insert(value:getPoint())
+	for key, value in pairs(units) do
+		table.insert(self.flexiborder, value:getPoint())
+		-- self.flexiborder.insert(value:getPoint())
 		count = count + 1
 	end
 	local V = getV2(self.flexiborder[2], self.flexiborder[1])
-	self.flexiborder[1] = {x = V.x * 1000000 + self.flexiborder[2].x, z = V.z * 1000000 + self.flexiborder[2].z}
+	table.insert(self.flexiborder, 1, {x = V.x * 1000000 + self.flexiborder[2].x, z = V.z * 1000000 + self.flexiborder[2].z})
+	count = count + 1
 
 	V = getV2(self.flexiborder[count - 1], self.flexiborder[count])
-	self.flexiborder[count] = {x = V.x * 1000000 + self.flexiborder[count - 1].x, z = V.z * 1000000 + self.flexiborder[count - 1].z}
+	table.insert(self.flexiborder, {x = V.x * 1000000 + self.flexiborder[count - 1].x, z = V.z * 1000000 + self.flexiborder[count - 1].z})
+	count = count + 1
 
 	local i = 1
 	while i < count do
 		trigger.action.lineToAll(-1, i + 1000, self.flexiborder[i], self.flexiborder[i + 1], {1, 0, 0, 1}, 1, true)
+		i = i + 1
 	end
 	trigger.action.textToAll(-1, 1000, {x = self.flexiborder[2].x - 6000, y = 0, z = self.flexiborder[2].z + 6000}, {1, 0, 0, 1}, {1, 0, 0, 0}, 20, true, 'PVP Border')
 end
@@ -67,7 +71,7 @@ function KillBorder.playerLoop(args)
 				--to be implemented
 			end
 		elseif math.abs(distance) < args.self.warnDistance then
-			trigger.action.outTextForUnit(args.playerObject:getID(), args.playerObject:getPlayerName() .. ' you are ' .. distance .. 'm away from the border of the allowed area, passing the border will be punished!', args.self.checkTime - 2, false)
+			trigger.action.outTextForUnit(args.playerObject:getID(), args.playerObject:getPlayerName() .. ' you are ' .. math.floor(distance) .. 'm away from the border of the allowed area, passing the border will be punished!', args.self.checkTime - 2, false)
 			trigger.action.outSoundForUnit(args.playerObject:getID(), args.self.sound)
 		else
 			args.self.groupArray[args.playerObject:getID()] = 0
@@ -91,7 +95,7 @@ function KillBorder.playerWarnLoop(args)
 			trigger.action.outSoundForUnit(args.playerObject:getID(), args.self.sound)
 			args.self.groupArray[args.playerObject:getID()] = 1
 		end
-		args.self.loopID = timer.scheduleFunction(args.self.playerLoop, args, timer.getTime() + args.self.checkTime)
+		args.self.loopID = timer.scheduleFunction(args.self.playerWarnLoop, args, timer.getTime() + args.self.checkTime)
 	end
 end
 --[[
@@ -121,10 +125,12 @@ end
 function KillBorder:getDistance(object)
 	local min = v2Magnitude(getV2(object:getPoint(), self.flexiborder[2]))
 	local P = 2
-	for key, value in self.flexiborder do
+	for key, value in pairs(self.flexiborder) do
+		local grid = coord.LLtoMGRS(coord.LOtoLL({x = value.x, y = 0, z = value.z}))
+		-- trigger.action.outTextForUnit(object:getID(), 'point ' .. key .. ', dist ' .. v2Magnitude(getV2(object:getPoint(), value)) .. ', exact point ' .. grid.UTMZone .. ' ' .. grid.MGRSDigraph .. ' ' .. grid.Easting .. ' ' .. grid.Northing, 10, false)
 		if v2Magnitude(getV2(object:getPoint(), value)) < min then
 			min = v2Magnitude(getV2(object:getPoint(), value))
-			Px = key
+			P = key
 		end
 	end
 	local A = P - 1
@@ -137,7 +143,11 @@ function KillBorder:getDistance(object)
 	local PCrelPA = getV2Relation(PC, PA)
 	local PCrelPB = getV2Relation(PC, PB)
 
-	if (PCrelPA < 0 and PCrelPB > 0) or ((getV2Relation(PA, PB) > 0) and ((PCrelPA > 0 and PCrelPB > 0) or (PCrelPA < 0 and PCrelPB < 0))) then
+	if (PCrelPA < 0 and PCrelPB > 0) then
+		-- trigger.action.outTextForUnit(object:getID(), 'case 1, point ' .. P .. ', dist ' .. min, 10, false)
+		return dist * -1
+	elseif ((getV2Relation(PA, PB) < 0) and ((PCrelPA > 0 and PCrelPB > 0) or (PCrelPA < 0 and PCrelPB < 0))) then
+		-- trigger.action.outTextForUnit(object:getID(), 'case 2, point ' .. P .. ', dist ' .. min, 10, false)
 		return dist * -1
 	end
 	return dist
@@ -148,12 +158,12 @@ function getV2(A, B)
 end
 
 function v2Magnitude(V)
-	return math.sqrt(V.x² + V.z²)
+	return math.sqrt(V.x^2 + V.z^2)
 end
 
 function getV2Relation(V1, V2)
 	--dot = a.x*-b.y + a.y*b.x
-	return V1.x * -V2.z + V1.z * V2.x
+	return V1.x * (-1 * V2.z) + V1.z * V2.x
 end
 
 function KillBorder:checkValidity(event)
@@ -181,6 +191,7 @@ function KillBorder:onEvent(event)
 	if event.id == 1 and self:checkValidity(event) == true then
 		self.weaponLoop({self = self, weaponObject = event.weapon})
 	elseif event.id == 15 and self:checkValidity(event) == true then
+		-- trigger.action.outTextForUnit(event.initiator:getID(), 'started playerloop for ' ..  event.initiator:getPlayerName(), 10, false)
 		self.playerLoop({self = self, playerObject = event.initiator})
 	elseif event.id == 15 then
 		self.playerWarnLoop({self = self, playerObject = event.initiator})
